74aea20933e8a5f603ca4deac05e27a89284835b
Gavin Hunter <77436251+hunterg05s@users.noreply.github.com>
2024-09-29 20:18:04 +0100

    Add files via upload

---
+++ docker-builds/metasploitable3-ub1404/entrypoint.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# clean exit
+trap "exit" SIGTERM
+
+# set password
+if [ $PASS ]; then
+  echo msfadmin:$PASS | chpasswd
+
+  # enable root
+  if [ $ROOT ]; then
+    echo root:$PASS | chpasswd
+  fi
+fi
+
+# launch services
+if [ $SSH ]; then
+  install -m 755 -d /var/run/sshd
+  /usr/sbin/sshd -D &
+fi
+
+if [ $CUPS ]; then
+  /usr/sbin/cups-browsed &
+  /usr/sbin/cupsd -f &
+fi
+
+if [ $FTP ]; then
+  /opt/proftpd/sbin/proftpd &
+fi
+
+if [ $HTTP ]; then
+  install -m 1777 -d /run/lock
+  apachectl start &
+fi
+
+if [ $IRC ]; then
+  /opt/unrealircd/Unreal3.2/unreal start &
+fi
+
+if [ $SMB ]; then
+  /usr/sbin/nmbd -D &
+  /usr/sbin/smbd -F &
+fi
+
+if [ $SQL ]; then
+  install -o mysql -g mysql -m 755 -d /run/mysql-default
+  /usr/sbin/mysqld --defaults-file=/etc/mysql-default/my.cnf &
+fi
+
+# see you at the restaurant at the end of the universe
+sleep infinity &
+wait
