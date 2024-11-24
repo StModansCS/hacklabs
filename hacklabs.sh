aa00ea1e7b9eb89cb72909b496f7691c3bb0d186
Gavin Hunter <77436251+hunterg05s@users.noreply.github.com>
2024-11-24 23:39:13 +0000

    Add files via upload

--- hacklabs.sh
+++ hacklabs.sh
@@ -2,68 +2,111 @@
 
 print_usage () {
     echo
-    echo 'Usage:    labs.sh COMMAND'
+    echo "Usage:    labs.sh COMMAND"
     echo
-    echo 'Control a class of hacklabs'
+    echo "Classroom hacklabs"
     echo
-    echo 'Commands:'
-    echo '    create              Setup hacklabs'
-    echo '    up                  Create and start hacklabs'
-    echo '    down                Stop and remove hacklabs'
+    echo "Commands:"
+    echo "    install             Install docker and hacklab files"
+    echo "    create              Setup hacklabs"
+    echo "    up                  Start hacklabs"
+    echo "    down                Stop hacklabs"
     echo
 }
 
+# get path of parent folder
 lab_path=$(dirname $0)
 
 if [ $# != 1 ]; then
     print_usage
-elif [ $1 == "create" ] ; then
-    read -p "How many hacklabs do you want to create?: " folder_count
-    IFS=$'\r\n' GLOBIGNORE='*' command eval 'passwords=($(cat passwords.txt))'
+elif [ $1 == "install" ] ; then
+    apt-get update
     
-    if [ ${#passwords[@]} -lt $folder_count ]; then
-        echo "Not enough entries in passwords.txt. Found ${#passwords[@]}."
+    # add Docker"s official GPG key:
+    apt-get install ca-certificates curl
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
+
+    # add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+      $(. /etc/os-release && echo "${VERSION_CODENAME}") stable" | \
+      tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get update
+
+    apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+    # download all other scripts
+    ##compose*yaml etc
+elif [ $1 == "create" ] ; then
+    read -p "How many hacklabs do you want to create?: " lab_count
+
+    # get passwords
+    IFS=$"\r\n" GLOBIGNORE="*" command eval "passwords=($(cat ${lab_path}/passwords.txt))"
+    
+    # check there are enough passwords
+    if [ ${#passwords[@]} -lt $lab_count ]; then
+        echo "Not enough entries in passwords.txt. Found ${#passwords[@]}"
         exit 1
     fi
     
-    for i in $(seq -f "%02g" 0 $((folder_count-1))); do
-        if [ -d "$lab_path/hacklab$i" ]; then
-            rm -rf "$lab_path/hacklab$i"
-        fi
+    # for each lab
+    for i in $(seq -f "%02g" 0 $((lab_count - 1))); do
+        # remove old hacklabs
+        rm -rf "${lab_path}/hacklab${i}"
+
+        # create docker compose file
+        mkdir "${lab_path}/hacklab${i}"
+        cp "${lab_path}/compose-template.yaml" "${lab_path}/hacklab${i}/compose-hacklab${i}.yaml"
         
-        mkdir $lab_path/hacklab$i
-        cp $lab_path/docker-template.yml "$lab_path/hacklab$i/docker-compose.yml"
-        
-        sed -i "s/container_name: hacklabXX_/container_name: hacklab${i}_/g" "$lab_path/hacklab$i/docker-compose.yml"
-        sed -i "s/: 172.0.X./: 172.0.$(echo "$i" | sed 's/^0//')./g" "$lab_path/hacklab$i/docker-compose.yml"
-        sed -i "s/PASS: myPass/PASS: ${passwords[$(echo "$i" | sed 's/^0//')]}/g" "$lab_path/hacklab$i/docker-compose.yml"
+        # edit compose file
+        sed -i "s/hacklabXXXX_/hacklab${i}_/g" "${lab_path}/hacklab${i}/compose-hacklab${i}.yaml"
+        sed -i "s/.XXXX./.$(echo "${i}" | sed "s/^0//")./g" "${lab_path}/hacklab${i}/compose-hacklab${i}.yaml"
+        sed -i "s/PASS: myPass/PASS: ${passwords[$(echo "${i}" | sed "s/^0//")]}/g" "${lab_path}/hacklab${i}/compose-hacklab${i}.yaml"
     done
 elif [ $1 == "up" ] ; then
-    folder_count=$(find "$lab_path" -maxdepth 1 -type d -name "hacklab??" | wc -l)
-    if [ $folder_count -eq 0 ]; then
-        echo "No hacklab folders found. Run 'labs.sh create' first."
+    # remove IP from hacklab server
+    ##ip address flush dev ens18
+
+    # get number of labs
+    lab_count=$(find "${lab_path}" -maxdepth 1 -type d -name "hacklab??" | wc -l)
+
+    # check there are labs to bring up
+    if [ $lab_count -eq 0 ]; then
+        echo "No hacklabs found. Run 'hacklabs.sh create' first."
         exit 1
     fi
     
-    for i in $(seq -f "%02g" 0 $((folder_count-1))); do
-        docker compose -f $lab_path/hacklab$i/docker-compose.yml up -d
+    # start hacklabs
+    for i in $(seq -f "%02g" 0 $((lab_count - 1))); do
+        docker compose -f "${lab_path}/hacklab${i}/compose-hacklab${i}.yaml" up -d
     done
-    #docker compose -f $lab_path/edge/docker-compose.yml up -d
+
     echo
-    echo 'All wrapped up here, sir. Will there be anything else?'
+    echo 'All wrapped up here, sir. Will there be anything else?'    
 elif [ $1 == "down" ]; then
-    folder_count=$(find "$lab_path" -maxdepth 1 -type d -name "hacklab??" | wc -l)
-    if [ $folder_count -eq 0 ]; then
+    # get number of labs
+    lab_count=$(find "${lab_path}" -maxdepth 1 -type d -name "hacklab??" | wc -l)
+
+    # check there are labs to bring down
+   if [ $lab_count -eq 0 ]; then
         echo "No hacklab folders found. Run 'labs.sh create' first."
         exit 1
     fi
     
-    for i in $(seq -f "%02g" 0 $((folder_count-1))); do
-        docker compose -f $lab_path/hacklab$i/docker-compose.yml down
+    # stop hacklabs
+    for i in $(seq -f "%02g" 0 $((lab_count - 1))); do
+        docker compose -f "${lab_path}/hacklab${i}/compose-hacklab${i}.yaml" down
     done
-    #docker compose -f $lab_path/edge/docker-compose.yml down
+
     echo
     echo 'The Clean Slate Protocol, sir?'
+
+    # restore IP to hacklab server
+    ##systemctl restart networking
 else
     print_usage
 fi
+
+
